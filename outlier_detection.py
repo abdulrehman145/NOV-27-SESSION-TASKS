f569d15440952cdcc4d5b0af061ee3409caca6c2
unknown <ranaabdulrehmanali145@gmail.com>
2024-11-13 09:27:20 +0500

    code for zscore

---
+++ outlier_detection.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy import stats
+
+
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+
+from sklearn.cluster import DBSCAN
+from sklearn.preprocessing import StandardScaler
+
+np.random.seed(42)
+n_values=np.random.normal(0,3,100)
+outliers=np.random.normal(0,5,100)
+data=np.concatenate([n_values,outliers])
+df=pd.DataFrame(data,columns=['values'])
+
+print("The values are given below:\n",df)
+df['zscore']=np.abs(stats.zscore(df['values']))
+print("The zscore of the values is below:\n",df)
