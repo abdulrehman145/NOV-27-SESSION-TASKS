26f62f7ad237d6b5638c594d197f76d68de9827c
unknown <ranaabdulrehmanali145@gmail.com>
2024-11-13 12:40:29 +0500

    code for outlier detection by IQR

--- outlier_detection.py
+++ outlier_detection.py
@@ -25,3 +25,14 @@ plt.figure(figsize=(10,5))
 sns.boxplot(df['values'])
 plt.title("Boxplot Visualization Of Data")
 plt.show()
+
+Q1=df['values'].quantile(0.25)
+Q3=df['values'].quantile(0.75)
+IQR=Q3-Q1
+
+lower_bound=(Q1-1.5)*IQR
+upper_bound=(Q3+1.5)*IQR
+
+outliers_detected_iqr=df((df['values']>lower_bound) | (df['values']<upper_bound))
+print("outliers detected by IQR Method:\n")
+print(outliers_detected_iqr)
