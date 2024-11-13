5b48b0165654c20afca91ecc60211f2ae5e95d70
unknown <ranaabdulrehmanali145@gmail.com>
2024-11-13 14:15:03 +0500

    outliers detection by DBSCAN

--- outlier_detection.py
+++ outlier_detection.py
@@ -44,3 +44,33 @@ plt.title("Box plot representing IQR outliers")
 plt.xlabel("Values")
 plt.legend()
 plt.show()
+
+scaler = StandardScaler()
+df["scaled_values"] = scaler.fit_transform(df[['values']])
+
+
+db = DBSCAN(eps=0.5, min_samples=5).fit(df[['scaled_values']])
+df['dbscan_labels'] = db.labels_
+
+
+outliers_dbscan = df[df['dbscan_labels'] == -1]
+print("Outliers detected by DBSCAN:")
+print(outliers_dbscan)
+
+
+plt.figure(figsize=(10, 5))
+
+
+plt.scatter(df[df['dbscan_labels'] != -1]['scaled_values'],
+            np.zeros(len(df[df['dbscan_labels'] != -1])),
+            color="blue", label="Clustered Points")
+
+
+plt.scatter(outliers_dbscan['scaled_values'],
+            np.zeros(len(outliers_dbscan)),
+            color="red", label="Outliers")
+
+plt.title("Outliers detected by using DBSCAN")
+plt.xlabel("Values")
+plt.legend()
+plt.show()
